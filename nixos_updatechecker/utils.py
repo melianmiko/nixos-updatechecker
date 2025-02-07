3be192e37a282319285253ca333bc63b2dfad0d1
Arik <BBArik@protonmail.com>
2025-02-07 11:46:37 -0500

    feat: add custom config directory

--- nixos_updatechecker/utils.py
+++ nixos_updatechecker/utils.py
@@ -1,7 +1,10 @@
+import os
+import re
 import threading
 
 from gi.repository import GLib
 
+DEFAULT_CONFIG_DIR = "/etc/nixos/"
 
 def ui_func(f):
     def wrapped(result, event, args, kwargs):
@@ -17,3 +20,12 @@ def ui_func(f):
         return result[0]
 
     return full
+
+def get_config_dir():
+    config_dir = DEFAULT_CONFIG_DIR
+    if (nix_path := os.environ.get("NIX_PATH")) is not None:
+      paths = re.split("=|:", nix_path)
+      idx = paths.index("nixos-config") + 1
+      if idx < len(paths):
+        config_dir = os.path.dirname(paths[idx])
+    return config_dir
